06865b63138b6eb5d0beb77c392284a69a01cb90
guiga <f.guilhermeferreirafs@gmail.com>
2023-08-29 00:06:34 -0300

    Finalize correção de bug

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
-const circles = document.querySelectorAll(".circle");
+const circle = document.querySelectorAll(".circle");
 
 let currentActive = 1;
 
 next.addEventListener("click", () => {
     currentActive++;
 
-    if (currentActive > circles.length) {
-        currentActive = circles.length;
+    if (currentActive > circle.length) {
+        currentActive = circle.length;
     }
     update();
 });
@@ -24,7 +24,7 @@ prev.addEventListener("click", () => {
 });
 
 function update () {
-    circles.forEach ((circle, idx) => {
+    circle.forEach ((circle, idx) => {
         if (idx < currentActive) {
             circle.classList.add("active");
         } else {
@@ -34,11 +34,11 @@ function update () {
 
     const actives = document.querySelectorAll(".active");
 
-    progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+    progress.style.width = ((actives.length - 1) / (circle.length - 1)) * 100 + "%";
 
     if (currentActive === 1 ) {
         prev.disabled = true;
-    } else if (currentActive === circles.length) {
+    } else if (currentActive === circle.length) {
         next.disabled = true;
     } else {
         prev.disabled = false;
